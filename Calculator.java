fcd0eb44b758e499c871742cd17270209c967793
JAMOLOVI <jamolbekjamolov50@gmail.com>
2025-06-10 08:55:17 +0500

    Initial commit with Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,6 @@
+public class Calculator {
+    public static int add(int a, int b) { return a + b; }
+    public static void main(String[] args) {
+        System.out.println("Sum: " + add(3, 4));
+    }
+}
